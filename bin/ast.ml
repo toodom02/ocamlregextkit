7a352d17c4e98efcf497a2ef27625f7f76af23cd
Dominic Too <toodom02@gmail.com>
2022-06-30 20:35:24 +0100

    print ast

--- bin/ast.ml
+++ bin/ast.ml
@@ -6,3 +6,17 @@ type re =
     | Concat of re * re     (* E·R *)
     | Star of re            (* E* *)
     | Empty                 (* ∅ *)
+
+(* |stringify_ast| -- converts regex ast into printable string *)
+let rec stringify_ast re = 
+    match re with
+          Skip -> ""
+        | Literal a -> "Literal " ^ a
+        | Epsilon -> "ε"
+        | Union (r1, r2) -> "Union (" ^ stringify_ast r1 ^ " , " ^ stringify_ast r2 ^ ")"
+        | Concat (r1, r2) -> "Concat (" ^ stringify_ast r1 ^ " , " ^ stringify_ast r2 ^ ")"
+        | Star r1 -> "Star (" ^ stringify_ast r1 ^ ")"
+        | Empty -> "∅"
+
+(* |print_ast| -- prints string representation of regex ast *)
+let print_ast re = print_string (stringify_ast re);  print_newline ()
--- bin/main.ml
+++ bin/main.ml
@@ -3,3 +3,5 @@ open Ast
 (* |parse| -- parse a string as a regular expression *)
 let parse s =
     Parser.regex Lexer.token (Lexing.from_string s)
+
+let () = print_ast (parse "(a + b)* + a.c + EPSILON")
