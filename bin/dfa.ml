5df5e66cd106bc68bdddbc54ebbe28e20372f31a
Dominic Too <toodom02@gmail.com>
2022-08-15 23:08:25 +0100

    Powerset construction for intersection then union

--- bin/dfa.ml
+++ bin/dfa.ml
@@ -1,9 +1,103 @@
 open Nfa
 
-type state = (int list)
+type state = int list
 type dfa = {
     states: state list; alphabet: string list; transitions: (state * string * state) list; start: state; accepting: state list
 }
+
+type product_state = state * state
+type product_dfa = {
+    states: product_state list; alphabet: string list; transitions: (product_state * string * product_state) list; start: product_state; accepting: product_state list
+}
+
+type product_product_state = product_state * product_state
+type product_product_dfa = {
+    states: product_product_state list; alphabet: string list; transitions: (product_product_state * string * product_product_state) list; start: product_product_state; accepting: product_product_state list
+}
+
+(* |dfa_compliment| -- returns the compliment of input dfa *)
+let dfa_compliment (m:dfa):dfa = 
+    {
+        states = m.states;
+        alphabet = m.alphabet;
+        transitions = m.transitions;
+        start = m.start;
+        accepting = List.filter (fun ss -> not (List.mem ss m.accepting)) m.states;  
+        (*possible issues here with ordering of lists. Should be fine for now as we don't change the order of any states*)
+        (* TODO: make sure states are always ordered? To ensure this is never a problem. Perhaps not worth the computation, though*)
+    }
+
+(* |list_union| -- returns union of the two input lists *)
+let rec list_union l1 l2 = 
+    match l2 with
+          [] -> l1
+        | x::xs -> if not (List.mem x l1) then list_union (x::l1) xs else list_union l1 xs
+
+let find_resulting_state initialState symbol transitions = 
+    let resultState = ref [] in
+    List.iter (fun (s,a,t) ->
+        if (s = initialState && a = symbol) then resultState := t
+    ) transitions;
+    !resultState
+
+(* |find_product_trans| -- returns { ((l,r),a,(l',r')) : (l,a,r) ∨ (l',a,r') } ish*)
+let find_product_trans cartStates fstTrans sndTrans alphabet = 
+    let newTrans = ref [] in
+    List.iter (fun (l,r) -> 
+        List.iter (fun a -> 
+            let lRes = find_resulting_state l a fstTrans and
+                rRes = find_resulting_state r a sndTrans in
+                    newTrans := ((l,r),a,(lRes,rRes))::!newTrans;
+        ) alphabet
+    ) cartStates;
+    !newTrans
+
+(* |product_intersection| -- returns the union of two input dfas, using the product construction (of a subset constructed dfa) *)
+let product_intersection (m1:dfa) (m2:dfa) : product_dfa =
+    let cartesianStates = List.concat (List.map (fun e1 -> List.map (fun e2 -> (e1,e2)) m2.states) m1.states) in
+    let unionAlphabet = list_union m1.alphabet m2.alphabet in
+    let cartTrans = find_product_trans cartesianStates m1.transitions m2.transitions unionAlphabet and
+        cartAccepting = List.filter (fun (l,r) -> List.mem l m1.accepting && List.mem r m2.accepting) cartesianStates in
+    {
+        states = cartesianStates;
+        alphabet = unionAlphabet;
+        transitions = cartTrans;
+        start = (m1.start, m2.start);
+        accepting = cartAccepting;
+    }
+
+let find_resulting_product_state initialState symbol transitions = 
+    let resultState = ref ([],[]) in
+    List.iter (fun (s,a,t) ->
+        if (s = initialState && a = symbol) then resultState := t
+    ) transitions;
+    !resultState
+
+(* |find_product_product_trans| -- returns { ((l,r),a,(l',r')) : (l,a,r) ∨ (l',a,r') } ish*)
+let find_product_product_trans cartStates fstTrans sndTrans alphabet = 
+    let newTrans = ref [] in
+    List.iter (fun (l,r) -> 
+        List.iter (fun a -> 
+            let lRes = find_resulting_product_state l a fstTrans and
+                rRes = find_resulting_product_state r a sndTrans in
+                    newTrans := ((l,r),a,(lRes,rRes))::!newTrans;
+        ) alphabet
+    ) cartStates;
+    !newTrans
+
+(* |product_union| -- returns the union of two input dfas, using the product construction (of a product constructed, subset constructed dfa) *)
+let product_union (m1:product_dfa) (m2:product_dfa) : product_product_dfa =
+    let cartesianStates = List.concat (List.map (fun e1 -> List.map (fun e2 -> (e1,e2)) m2.states) m1.states) in
+    let unionAlphabet = list_union m1.alphabet m2.alphabet in
+    let cartTrans = find_product_product_trans cartesianStates m1.transitions m2.transitions unionAlphabet and
+        cartAccepting = List.filter (fun (l,r) -> List.mem l m1.accepting || List.mem r m2.accepting) cartesianStates in
+    {
+        states = cartesianStates;
+        alphabet = unionAlphabet;
+        transitions = cartTrans;
+        start = (m1.start, m2.start);
+        accepting = cartAccepting;
+    }
 
 (* |powerset| -- returns the powerset of input list *)
 let rec powerset xs =
@@ -67,10 +161,39 @@ let nfa_to_dfa (n: nfa): dfa =
             accepting = newaccepting;
         }
 
-(* |print_dfa| -- prints out nfa representation *)
-let print_dfa n = 
-    print_string "states: "; List.iter (fun ss -> print_string "[ "; List.iter (fun s -> print_int s; print_char ' ') ss; print_char ']') n.states; print_newline ();
+(* the following functions are used to print out representations of states/dfas *)
+
+let rec print_subset_state (n:state) =
+    print_string "[ "; List.iter (fun s -> print_int s; print_char ' ') n; print_string "]"
+
+and print_product_state (n:product_state) =
+    let (l,r) = n in
+    print_string "( "; print_subset_state l; print_string " , "; print_subset_state r; print_string " )";
+
+and print_product_product_state (n:product_product_state) = 
+    let (l,r) = n in
+    print_string "( "; print_product_state l; print_string " , "; print_product_state r; print_string " )";
+
+(* |print_dfa| -- prints out dfa representation, formed by subset construction *)
+and print_dfa (n:dfa) = 
+    print_string "states: "; List.iter (fun ss -> print_subset_state ss) n.states; print_newline ();
     print_string "alphabet: "; List.iter (fun a -> print_string a; print_char ' ') n.alphabet; print_newline ();
-    print_string "start: "; print_string "[ "; List.iter (fun s -> print_int s; print_char ' ') n.start; print_char ']'; print_newline ();
-    print_string "accepting: "; List.iter (fun ss -> print_string "[ "; List.iter (fun s -> print_int s; print_char ' ') ss; print_char ']') n.accepting; print_newline ();
-    print_string "transitions: "; print_newline(); List.iter (fun (ss,a,tt) -> print_string "    "; print_string "[ "; List.iter (fun s -> print_int s; print_char ' ') ss; print_char ']'; print_string ("\t--"^a^"-->\t"); print_string "[ "; List.iter (fun t -> print_int t; print_char ' ') tt; print_char ']'; print_newline ()) n.transitions;
+    print_string "start: "; print_subset_state n.start; print_newline ();
+    print_string "accepting: "; List.iter (fun ss -> print_subset_state ss) n.accepting; print_newline ();
+    print_string "transitions: "; print_newline (); List.iter (fun (ss,a,tt) -> print_string "    "; print_subset_state ss; print_string ("\t--"^a^"-->\t"); print_subset_state tt; print_newline ()) n.transitions;
+
+(* |print_product_dfa| -- prints out product dfa representation *)
+and print_product_dfa (n:product_dfa) = 
+    print_string "states: "; List.iter (fun s -> print_product_state s) n.states; print_newline ();
+    print_string "alphabet: "; List.iter (fun a -> print_string a; print_char ' ') n.alphabet; print_newline ();
+    print_string "start: "; print_product_state n.start; print_newline ();
+    print_string "accepting: "; List.iter (fun s -> print_product_state s) n.accepting; print_newline ();
+    print_string "transitions: "; print_newline (); List.iter (fun (l,a,r) -> print_string "    "; print_product_state l; print_string ("\t--"^a^"-->\t"); print_product_state r; print_newline ()) n.transitions;
+
+(* |print_product_product_dfa| -- prints out dfa formed by product construction of two product constructed dfas *)
+and print_product_product_dfa (n:product_product_dfa) =
+    print_string "states: "; List.iter (fun s -> print_product_product_state s) n.states; print_newline ();
+    print_string "alphabet: "; List.iter (fun a -> print_string a; print_char ' ') n.alphabet; print_newline ();
+    print_string "start: "; print_product_product_state n.start; print_newline ();
+    print_string "accepting: "; List.iter (fun s -> print_product_product_state s) n.accepting; print_newline ();
+    print_string "transitions: "; print_newline (); List.iter (fun (l,a,r) -> print_string "    "; print_product_product_state l; print_string ("\t--"^a^"-->\t"); print_product_product_state r; print_newline ()) n.transitions;
--- bin/main.ml
+++ bin/main.ml
@@ -13,17 +13,50 @@ let main () =
         while true do
             print_string "? "; flush stdout;
             let line = input_line stdin in
+            print_string "? "; flush stdout;
+            let line2 = input_line stdin in
             try
                 let re = parse line in
                 let simp_re = simplify re in
                 print_ast simp_re;
                 print_newline ();
                 let nfa2 = construct_nfa simp_re in
-                print_nfa nfa2;
-                print_newline ();
+                (* print_nfa nfa2; *)
+                (* print_newline (); *)
                 let dfa2 = nfa_to_dfa nfa2 in
-                print_dfa dfa2;
+                (* print_dfa dfa2; *)
+                (* print_newline (); *)
+                let comp = dfa_compliment dfa2 in
+                (* print_dfa comp; *)
+                (* print_newline (); *)
+
+                let re2 = parse line2 in
+                let simp_re2 = simplify re2 in
+                print_ast simp_re2;
                 print_newline ();
+                let nfa22 = construct_nfa simp_re2 in
+                (* print_nfa nfa2; *)
+                (* print_newline (); *)
+                let dfa22 = nfa_to_dfa nfa22 in
+                (* print_dfa dfa22; *)
+                (* print_newline (); *)
+                let comp2 = dfa_compliment dfa22 in
+                (* print_dfa comp; *)
+                (* print_newline (); *)
+
+                let fst_and_not_snd = product_intersection dfa2 comp2 and
+                    not_fst_and_snd = product_intersection comp dfa22 in
+
+                let product_dfa = product_union fst_and_not_snd not_fst_and_snd in
+                
+                print_product_dfa fst_and_not_snd;
+                print_newline ();
+                print_product_dfa not_fst_and_snd;
+                print_newline ();
+
+                print_product_product_dfa product_dfa;
+                print_newline ();
+                
             with 
                  _ -> print_string "syntax error"; print_newline ()
         done
