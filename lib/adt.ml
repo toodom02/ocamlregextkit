5064c6752f7bc657a5198283ffdb780a6fd27e61
Dominic Too <toodom02@gmail.com>
2023-08-19 23:59:43 +0100

    feat: remove extraneous fields from ADT

--- lib/adt.ml
+++ lib/adt.ml
@@ -2,22 +2,32 @@ type 't automata = {
   states : 't list;
   alphabet : string list;
   transitions : ('t, (string, 't) Hashtbl.t) Hashtbl.t;
-  transitions_list : ('t * string * 't) list;
-  inverse_transitions : ('t, (string, 't) Hashtbl.t) Hashtbl.t;
   start : 't;
   accepting : ('t, bool) Hashtbl.t;
-  accepting_list : 't list;
 }
 
 let get_states m = m.states
 let get_alphabet m = m.alphabet
-let get_transitions m = m.transitions_list
+
+let get_transitions m =
+  Hashtbl.fold
+    (fun s ats acc -> Hashtbl.fold (fun a t acc' -> (s, a, t) :: acc') ats acc)
+    m.transitions []
+
 let get_start m = m.start
-let get_accepting m = m.accepting_list
+
+let get_accepting m =
+  Hashtbl.fold (fun k v acc -> if v then k :: acc else acc) m.accepting []
+
 let get_next_states m s a = Hashtbl.find_all (Hashtbl.find m.transitions s) a
 
 let get_prev_states m t a =
-  Hashtbl.find_all (Hashtbl.find m.inverse_transitions t) a
+  Hashtbl.fold
+    (fun s v acc ->
+      Hashtbl.fold
+        (fun a' t' acc' -> if a = a' && t = t' then s :: acc' else acc')
+        v acc)
+    m.transitions []
 
 let is_accepting m s = Option.is_some (Hashtbl.find_opt m.accepting s)
 
@@ -38,28 +48,18 @@ let get_reachable_states m =
 let create_automata qs alph tran init fin =
   let length = List.length qs in
   let transitions = Hashtbl.create length in
-  let inverse_transitions = Hashtbl.create length in
   List.iter
     (fun s ->
       let tbl = Hashtbl.create (List.length alph) in
-      let tbl2 = Hashtbl.create (List.length alph) in
-      Hashtbl.add transitions s tbl;
-      Hashtbl.add inverse_transitions s tbl2)
+      Hashtbl.add transitions s tbl)
     qs;
-  List.iter
-    (fun (s, a, t) ->
-      Hashtbl.add (Hashtbl.find transitions s) a t;
-      Hashtbl.add (Hashtbl.find inverse_transitions t) a s)
-    tran;
+  List.iter (fun (s, a, t) -> Hashtbl.add (Hashtbl.find transitions s) a t) tran;
   let accepting = Hashtbl.create length in
   List.iter (fun s -> Hashtbl.add accepting s true) fin;
   {
     states = qs;
     alphabet = List.sort compare alph;
     transitions;
-    transitions_list = tran;
-    inverse_transitions;
     start = init;
     accepting;
-    accepting_list = fin;
   }
